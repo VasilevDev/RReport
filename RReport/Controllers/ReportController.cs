c850ed44599d03985ce72b689efce605c8c0f14d
Alexey Vasilev <valexey.dev@gmail.com>
2018-05-21 11:07:32 +0600

    реализовал создание отчета с источником данных и его заполнение

--- RReport/Controllers/ReportController.cs
+++ RReport/Controllers/ReportController.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Npgsql;
 using RReport.Database;
 using RReport.Models;
 using Stimulsoft.Report;
+using Stimulsoft.Report.Dictionary;
 using Stimulsoft.Report.Mvc;
 using System;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace RReport.Controllers
 {
@@ -24,6 +28,31 @@ namespace RReport.Controllers
 			try
 			{
 				StiReport report = new StiReport();
+				StiDatabase database = new StiPostgreSQLDatabase("ownRadioRdev", "Server=localhost;Port=5432;User Id = postgres;Password=postgres;Database=ownRadioRdev;");
+				StiDataSource datasource = new StiPostgreSQLSource("ownRadioRdev", "tracks", "tracks", "select * from tracks", true, false);
+
+				report.Dictionary.DataSources.Add(datasource);
+				report.Dictionary.Databases.Add(database);
+
+				using(var connection = new NpgsqlConnection("Server=localhost;Port=5432;User Id = postgres;Password=postgres;Database=ownRadioRdev;"))
+				{
+					NpgsqlDataAdapter adapter = new NpgsqlDataAdapter("select * from tracks", connection);
+
+					DataTable dataTableCategories = new DataTable();
+					adapter.Fill(dataTableCategories);
+
+					foreach (DataColumn col in dataTableCategories.Columns)
+					{
+						datasource.Columns.Add(col.ColumnName, col.DataType);
+					}
+				}
+
+				report.Dictionary.Synchronize();
+
+				return Ok(JObject.Parse(report.SaveToJsonString()));
+
+				/*
+				report.RegData("PostgreSQL", new SqlConnection());
 				report.LoadDocument(StiNetCoreHelper.MapPath(this, $"Reports/{reportRequest.Report}"));
 
 				StiNetCoreActionResult result = null;
@@ -54,7 +83,7 @@ namespace RReport.Controllers
 
 				report.LoadDocumentFromJson(JsonConvert.SerializeObject(reportJson));
 
-				return StiNetCoreReportResponse.PrintAsHtml(report);
+				return StiNetCoreReportResponse.PrintAsHtml(report);*/
 			}
 			catch(Exception ex)
 			{

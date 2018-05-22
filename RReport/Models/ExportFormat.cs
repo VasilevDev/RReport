6a049007f4c12d6ad71f037f7ae89c78f6997a09
Alexey Vasilev <valexey.dev@gmail.com>
2018-05-22 14:27:13 +0600

    вынес определение в каком формате возвращать отчет в отдельный класс

---
+++ RReport/Models/ExportFormat.cs
@@ -0,0 +1,36 @@
+﻿using Stimulsoft.Report;
+using Stimulsoft.Report.Mvc;
+using System;
+
+namespace RReport.Models
+{
+	public class ExportFormat
+	{
+		public StiNetCoreActionResult ExportReport(StiReport report, string format)
+		{
+			StiNetCoreActionResult result;
+
+			switch (format)
+			{
+				case "pdf":
+					result = StiNetCoreReportResponse.ResponseAsPdf(report);
+					break;
+				case "html":
+					result = StiNetCoreReportResponse.PrintAsHtml(report);
+					break;
+				case "xls":
+					result = StiNetCoreReportResponse.ResponseAsXls(report);
+					break;
+				case "xml":
+					result = StiNetCoreReportResponse.ResponseAsXml(report);
+					break;
+				
+					//todo
+				default:
+					throw new Exception("Недопустимый формат файла");
+			}
+
+			return result;
+		}
+	}
+}

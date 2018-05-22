92988d07af085a204fdd4d85e33cab9ebd4232d2
Alexey Vasilev <valexey.dev@gmail.com>
2018-05-22 14:28:34 +0600

    изменил реализацию метода GetReport в связи с добавлением классов - поставщиков данных

--- RReport/Controllers/ReportController.cs
+++ RReport/Controllers/ReportController.cs
@@ -1,15 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Npgsql;
 using RReport.Database;
 using RReport.Models;
+using RReport.Models.DataProviders;
 using Stimulsoft.Report;
-using Stimulsoft.Report.Dictionary;
 using Stimulsoft.Report.Mvc;
 using System;
-using System.Data;
-using System.Data.SqlClient;
 
 namespace RReport.Controllers
 {
@@ -25,70 +21,44 @@ namespace RReport.Controllers
 		[HttpPost]
 		public IActionResult GetReport([FromBody]ReportRequestDto reportRequest)
 		{
+			StiReport report = new StiReport();
+			IDataProvider provider = null;
+
 			try
 			{
-				StiReport report = new StiReport();
-				StiDatabase database = new StiPostgreSQLDatabase("ownRadioRdev", "Server=localhost;Port=5432;User Id = postgres;Password=postgres;Database=ownRadioRdev;");
-				StiDataSource datasource = new StiPostgreSQLSource("ownRadioRdev", "tracks", "tracks", "select * from tracks", true, false);
+				report.Load(StiNetCoreHelper.MapPath(this, $"Reports/{reportRequest.Report}"));
 
-				report.Dictionary.DataSources.Add(datasource);
-				report.Dictionary.Databases.Add(database);
-
-				using(var connection = new NpgsqlConnection("Server=localhost;Port=5432;User Id = postgres;Password=postgres;Database=ownRadioRdev;"))
+				if (reportRequest.DataProvider.ToLower() == "xml")
 				{
-					NpgsqlDataAdapter adapter = new NpgsqlDataAdapter("select * from tracks", connection);
+					provider = new XmlFileDataProvider(reportRequest.Data["filename"].Value<string>());
+					report.RegData(provider.GetData());
+				} 
+				else if (reportRequest.DataProvider.ToLower() == "database")
+				{
+					StimulsoftRepository repository = new StimulsoftRepository();
+					ReportSettings reportSettings = repository.GetReportSettings(reportRequest.Report);
 
-					DataTable dataTableCategories = new DataTable();
-					adapter.Fill(dataTableCategories);
-
-					foreach (DataColumn col in dataTableCategories.Columns)
+					foreach (var tableName in reportSettings.TableNames)
 					{
-						datasource.Columns.Add(col.ColumnName, col.DataType);
+						provider = new DatabaseDataProvider(tableName, reportSettings.ConnectionString, reportRequest.Data);
+						report.RegData(provider.GetData());
 					}
 				}
-
-				report.Dictionary.Synchronize();
-
-				return Ok(JObject.Parse(report.SaveToJsonString()));
-
-				/*
-				report.RegData("PostgreSQL", new SqlConnection());
-				report.LoadDocument(StiNetCoreHelper.MapPath(this, $"Reports/{reportRequest.Report}"));
-
-				StiNetCoreActionResult result = null;
-
-				switch (reportRequest.Output.ToLower())
+				else if(reportRequest.DataProvider.ToLower() == "url")
 				{
-					case "pdf":
-						result = StiNetCoreReportResponse.ResponseAsPdf(report);
-					break;
-					case "html":
-						result = StiNetCoreReportResponse.PrintAsHtml(report);
-					break;
-					case "xls":
-						result = StiNetCoreReportResponse.ResponseAsXls(report);
-						break;
-
-					default:
-						throw new Exception("Недопустимый формат файла");
+					provider = new UrlDataProvider("URL");
+					report.RegData(provider.GetData());
 				}
-
-				var reportJson = JObject.Parse(report.SaveToJsonString());
-				var dataJson = JObject.FromObject(reportRequest.Data);
-
-				foreach (var item in dataJson)
-				{
-					reportJson[item.Key] = item.Value;
-				}
-
-				report.LoadDocumentFromJson(JsonConvert.SerializeObject(reportJson));
-
-				return StiNetCoreReportResponse.PrintAsHtml(report);*/
 			}
 			catch(Exception ex)
 			{
 				return StatusCode(500, ex.Message);
 			}
+
+			ExportFormat exporter = new ExportFormat();
+			return exporter.ExportReport(report, reportRequest.Output.ToLower());
+
+			//return StiNetCoreReportResponse.ResponseAsPdf(report);
 		}
 	}
 }
